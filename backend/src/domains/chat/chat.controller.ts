e7c681ef2f9b772437013cce35626a14c6b99771
Romain Capolunghi <capolunghiromain@gmail.com>
2024-10-18 15:50:57 +0200

    add input param

--- backend/src/domains/chat/chat.controller.ts
+++ backend/src/domains/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { Message } from '@anthropic-ai/sdk/resources';
 
 import { ChatService } from './chat.service';
@@ -8,7 +8,7 @@ export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post()
-  async askAI(): Promise<Message> {
-    return this.chatService.askAI();
+  async askAI(@Body() chatInput: string): Promise<Message> {
+    return this.chatService.askAI(chatInput);
   }
 }
